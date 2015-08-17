174e3bd6829a9e4e49429e3c5c2b2c2ae41b88fc
Pradyumna <pradyumna2905@gmail.com>
2015-08-17 16:33:32 -0700

    display digits on UILabel

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    var userIsTyping = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsTyping {
+            display.text! = display.text! + digit
+        } else {
+            display.text! = digit
+            userIsTyping = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+}
 }
 
