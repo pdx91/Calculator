71f52e5c3e60fee9281957c2d2b0ac6db4d449ed
Pradyumna <pradyumna2905@gmail.com>
2015-08-17 17:16:22 -0700

    Add enter and clear buttons Add basic calculations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController
 
     @IBOutlet weak var display: UILabel!
     var userIsTyping = false
+    var opStack = [Double]()
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -20,10 +21,53 @@ class ViewController: UIViewController
         if userIsTyping {
             display.text! = display.text! + digit
         } else {
-            display.text! = digit
+            display.text = digit
             userIsTyping = true
+        }
     }
 
-}
+
+    @IBAction func operation(sender: UIButton) {
+        let calculation = sender.currentTitle!
+        enter()
+        switch calculation {
+        case "÷": performOperation() { $1 / $0 }
+        case "×": performOperation() { $0 * $1 }
+        case "−": performOperation() { $1 - $0 }
+        case "+": performOperation() { $0 + $1 }
+        default: break
+        }
+   }
+
+    func performOperation(operation: (Double, Double) -> Double) {
+        if opStack.count >= 2 {
+            displayValue = operation(opStack.removeLast(), opStack.removeLast())
+        }
+    }
+
+    var displayValue: Double {
+        get {
+            return (display.text! as NSString).doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsTyping = false
+        opStack.append(displayValue)
+        println("opStack = \(opStack)")
+    }
+    
+    
+    @IBAction func clear() {
+        opStack.removeAll()
+        println("opStack = \(opStack)")
+        display.text = "0"
+    }
+    
+
+
 }
 
